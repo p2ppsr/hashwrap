4590248e59889d81aaa0e6ad68ba283bd3bc4b11
Tone Engel <tone@kizmet.org>
2024-07-17 09:35:05 -0400

    switch to getEnvelopeForTransaction in cwi-external-services

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { getMerkleProofFromWhatsOnChain } = require('cwi-external-services')
+const { CwiExternalServices, getEnvelopeForTransaction } = require('cwi-external-services')
 const bsv = require('babbage-bsv')
 const axios = require('axios')
 const { toBEEFfromEnvelope } = require('@babbage/sdk-ts')
-const { toString } = require('cwi-base')
+const { asString } = require('cwi-base')
 
 /**
  * Returns an SPV envelope given the TXID of the target transaction.
@@ -46,84 +46,12 @@ const hashwrap = async (txid, options = {}) => {
   if (txid.length !== 64 || !/[0-9a-f]{64}/g.test(txid)) {
     throw new Error('Invalid TXID')
   }
-  const wocNet = options.network === 'testnet' ? 'test' : 'main'
-  const { data: rawTx } = await axios.get(
-    `https://api.whatsonchain.com/v1/bsv/${wocNet}/tx/${txid}/hex`
-  )
-  if (!rawTx) {
-    throw new Error(`Could not find transaction on WhatsOnChain: ${txid}`)
-  }
+  const chain = options.network === 'testnet' ? 'test' : 'main'
 
-  const proof = await getMerkleProofFromWhatsOnChain(txid, wocNet)
+  const opts = CwiExternalServices.createDefaultOptions()
+  const services = new CwiExternalServices(opts)
+  let envelope = await getEnvelopeForTransaction(services, chain, txid)
   
-  let envelope = undefined
-
-  if (proof) {
-    envelope = {
-      rawTx,
-      proof: {
-        txOrId: proof.txOrId,
-        target: proof.target,
-        targetType: proof.targetType,
-        nodes: proof.nodes,
-        index: proof.index
-      }
-    }
-  } else {
-    let provider = 'mapi.gorillapool.io'
-    let headers = {}
-    const apiKey = options.taalApiKey
-
-    if (options.network === 'testnet') {
-      if (!apiKey) {
-        throw new Error('Taal API key required in testnet!')
-      }
-      if (!apiKey.startsWith('testnet_')) {
-        throw new Error('Taal API key must be a testnet key for testnet')
-      }
-    }
-
-    if (apiKey) {
-      provider = 'mapi.taal.com'
-      headers = { headers: { Authorization: apiKey } }
-    }
-
-    const { data: mapiResponse } = await axios.get(
-      `https://${provider}/mapi/tx/${txid}`, headers
-    )
-    const payloadHash = bsv.crypto.Hash.sha256(
-      Buffer.from(mapiResponse.payload)
-    )
-    const signature = bsv.crypto.Signature.fromString(mapiResponse.signature)
-    const publicKey = bsv.PublicKey.fromString(mapiResponse.publicKey)
-    if (bsv.crypto.ECDSA.verify(payloadHash, signature, publicKey) !== true) {
-      throw new Error(
-        `Inalid mAPI signature for TXID: ${txid}, payload: ${mapiResponse.payload}, publicKey: ${mapiResponse.publicKey}, signature: ${mapiResponse.signature}`
-      )
-    }
-    const payload = JSON.parse(mapiResponse.payload)
-    if (payload.txid !== txid) {
-      throw new Error(
-        `Invalid mAPI response, expected a response for TXID ${txid} but got one for ${payload.txid} instead`
-      )
-    }
-    if (payload.returnResult !== 'success') {
-      throw new Error(`Invalid mAPI status response for TXID: ${txid}, returnResult: ${payload.returnResult}, resultDescription: ${payload.resultDescription}`)
-    }
-    const inputs = {}
-    const tx = new bsv.Transaction(rawTx)
-    for (const input of tx.inputs) {
-      const txid = input.prevTxId.toString('hex')
-      if (inputs[txid]) continue
-      inputs[txid] = await hashwrap(txid, options)
-    }
-    envelope = {
-      rawTx,
-      mapiResponses: [mapiResponse],
-      inputs
-    }
-  }
-
   if (envelope) {
     if (options.format === 'beefHex') {
       const r = toBEEFfromEnvelope(envelope)
